f3a0ef5dad29007a6169f07cb11d2e10955359f4
UncleTed666 <56354720+UncleTed666@users.noreply.github.com>
2019-10-10 17:37:12 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,2 +1,19 @@
+import argparse
+
 x = 10
 print(x**2)
+
+def main():
+  parser = argparse.ArgumentParser(prog='Script to run')
+  parser.add_argument('--param', nargs='?', help='"X" param')
+
+  args = parser.parse_args()
+  run(**vars(args))
+
+def run(param=None):
+  if param:
+    return param**2
+  
+
+if name == '__main__':
+  main()
